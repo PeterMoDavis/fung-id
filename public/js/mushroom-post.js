78e3774e21a9a6c391fa06ed6340dbbbc3ec328a
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-04-18 13:12:41 -0400

    added some js

--- public/js/mushroom-post.js
+++ public/js/mushroom-post.js
@@ -5,9 +5,21 @@ const descriptionInput = document.querySelector("#description");
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const mushroomName = mushroomNameInput.value;
-  const description = descriptionInput.value;
+  const mushroomName = mushroomNameInput.value.trim();
+  const description = descriptionInput.value.trim();
 
+  if(mushroomName && description){
+    const response = await fetch("/api/users/login", {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    if (response.ok) {
+      document.location.replace('/api/users/login');
+  } else {
+      alert('Failed to log in.');
+  }
+  }
   alert(mushroomName);
   alert(description);
 });
